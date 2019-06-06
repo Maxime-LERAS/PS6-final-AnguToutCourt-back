c319f38a39cd7e20d6da97faaf28373e15530361
Maxime LERAS <maxime.leras@etu.unice.fr>
2019-06-06 10:30:45 +0200

    Correction erreur de frappe

--- app/api/reviews/index.js
+++ app/api/reviews/index.js
@@ -47,7 +47,7 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   try {
-    res.status(200).json(Profile.update(req.params.id, req.body));
+    res.status(200).json(Review.update(req.params.id, req.body));
   } catch (err) {
     if (err.name === 'NotFoundError') {
       res.status(404).end();
